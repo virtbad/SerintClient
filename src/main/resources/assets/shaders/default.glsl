ad9b09fd08cd787050662848f7ab590e7fd55e1b
VirtCode <41426325+VirtCode@users.noreply.github.com>
2021-03-21 11:21:06 +0100

    Default Shaders and Textures

---
+++ src/main/resources/assets/shaders/default.glsl
@@ -0,0 +1,20 @@
+#type vertex
+#version 330
+
+layout (location=0) in vec2 pos;
+
+uniform mat4 worldMatrix;
+uniform mat4 viewMatrix;
+
+void main() {
+    gl_Position = viewMatrix * (worldMatrix * vec4(pos.x, pos.y, 0.0, 1.0));
+}
+
+#type fragment
+#version 330
+
+out vec4 FragColor;
+
+void main(){
+    FragColor = vec4(0f, 1f, 0.5f, 1f); // Colour that only should appear here, so you can decide whether it is the default shader or not
+}
