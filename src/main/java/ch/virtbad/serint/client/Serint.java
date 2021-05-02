940c04fe8d388599ad98c081f3f9f8b4445b1249
VirtCode <41426325+VirtCode@users.noreply.github.com>
2021-05-02 15:55:35 +0200

    Version Bump

--- src/main/java/ch/virtbad/serint/client/Serint.java
+++ src/main/java/ch/virtbad/serint/client/Serint.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class Serint {
-    public static final String VERSION = "0.9-beta";
+    public static final String VERSION = "1.0";
 
     NetworkHandler network;
     Communications communications;
